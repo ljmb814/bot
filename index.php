65d5677ed64cfd96394b261c84e6991d20a32ac4
ljmb814 <163157562+ljmb814@users.noreply.github.com>
2024-03-12 11:10:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,14 @@
 <?php
-$token = '7088590956:AAHk81FxuN4eUWPb-ttFKiBT06HRfOASsbI';
-$website = 'https://api.telegram.org/bot'.$token;
+$botToken = "your_token";
+$website = "https://api.telegram.org/bot".$botToken;
 
-$input = file_get_contents('php://input');
-$update = json_decode($input, TRUE);
+$update = file_get_contents("php://input");
+$update = json_decode($update, TRUE);
 
-$chatId = $update['message']['chat']['id'];
-$message = $update['message']['text'];
+$chatId = $update["message"]["chat"]["id"];
+$text = $update["message"]["text"];
 
-switch($message) {
-    case '/start':
-        $response = 'Me has iniciado';
-        sendMessage($chatId, $response);
-        break;
-    case '/info':
-        $response = 'Hola! Soy @trecno_bot';
-        sendMessage($chatId, $response);
-        break;
-    default:
-        $response = 'No te he entendido';
-        sendMessage($chatId, $response);
-        break;
-}
-
-function sendMessage($chatId, $response) {
-    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
-    file_get_contents($url);
+if($text == "/start") {
+    file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=Perfecto, has iniciado el bot");
 }
 ?>
